4c470d775becf274ccb669bd006790ed7f9974f6
InderdeepSync <buntycool1998@gmail.com>
2022-03-03 20:42:34 -0500

    Add personal Information

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,18 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'Python',
+    'Node.js',
+    'Django',
+    'Docker',
+    'Amazon Web Services',
+    'SQL',
+    'Java',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,30 +147,25 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Inderdeep and I enjoy solving complex, challenging problems and
+              building innovative products that people love.
             </p>
-
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              My interest in software engineering started back in 2019 when I embarked upon building
+              an affiliate-marketing platform{' '}
+              <a href="https://bestviewsreviews.com/">BestViewsReviews</a> - turns out hacking
+              together a custom ecommerce web application was exactly what I needed to spark my
+              passion and curiosity in IT.
             </p>
-
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Fast-forward to today, and I’ve had the privilege of working at an established{' '}
+              <a href="https://birdeye.com/"> product company</a>, a buzzing{' '}
+              <a href="https://www.shorthillstech.com/"> start-up</a> and several student-led
+              initiatives and projects.
+            </p>
+            <p>
+              My main focus these days is building accessible, inclusive products and digital
+              experiences for a variety of clients.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,32 +59,29 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Inderdeep Singh.</h2>;
+  const three = <h3 className="big-heading">Crafting Apps to perfection.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a full-stack software engineer specializing in designing and building stellar digital
+        experiences.
+      </p>
+      <p>
+        Having obtained a Bachelor of Engineering in Computer Science from{' '}
+        <a href="https://www.bits-pilani.ac.in/" target="_blank" rel="noreferrer">
+          BITS Pilani (Pilani Campus)
+        </a>{' '}
+        in 2020, I’m currently pursuing the Master's Degree Program in Computer Science at{' '}
+        <a href="https://www.ubishops.ca/" target="_blank" rel="noreferrer">
+          Bishop's University
         </a>
         .
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
